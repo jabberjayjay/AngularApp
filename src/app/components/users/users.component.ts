02b246ac3e5fd462ec91978fd58d4c06b3df0d8e
JOHNATHAN JOYCE <G00007904@gmit.ie>
2018-11-29 20:29:06 +0000

    Commit with user details, making user data simpler for db

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {User} from '../../models/User';
+import { User } from '../../models/User';
 
 @Component({
   selector: 'app-users',
@@ -8,94 +8,144 @@ import {User} from '../../models/User';
 })
 export class UsersComponent implements OnInit {
 
-  
+  user: User = {
+    firstName: '',
+    lastName: '',
+    age: null,
+    address: {
+      street: '',
+      city: '',
+      state: ''
+
+    }
+
+  }
   users: User[];
-  loaded : boolean = false;
-  showExtended: boolean = false;
-  enableAdd: boolean = true;
-  currentClasses = {};
-  currentStyles = {};
+  loaded: boolean = false;
+  showExtended: boolean = true;
+  enableAdd: boolean = false;
+  showUserForm: boolean = true;
+  // currentClasses = {};
+  // currentStyles = {};
 
 
   constructor() { }
 
   ngOnInit() {
     console.log('init...');
-    
-      this.users = [
-        {
-          firstName: 'John',
-          lastName: 
+
+    this.users = [
+      {
+        firstName: 'John',
+        lastName:
           'Doe',
-          age: 30,
-          address: {
-            street : '50 main st',
-            city : 'Boston',
-            state: 'MA'
-          },
-          image: 'https://placeimg.com/300/300/people/3',
-          isActive : true,
-          balance: 100,
-          registered: new Date('01/02/2018 08:30:00')
+        age: 30,
+        address: {
+          street: '50 main st',
+          city: 'Boston',
+          state: 'MA'
         },
-        {
-          firstName: 'Kevin',
-          lastName:  'Johnson',
-          age: 34,
-          address: {
-            street : '20 School st',
-            city : 'Lynn',
-            state: 'MA'
-          },
-          image: 'https://placeimg.com/300/300/people/1',
-          isActive : false,
-          balance: 200,
-          registered: new Date('03/11/2018 08:30:00')
+        // image: 'https://placeimg.com/300/300/people/3',
+        isActive: true,
+        // balance: 100,
+        registered: new Date('01/02/2018 08:30:00'),
+        hide: true
+      },
+      {
+        firstName: 'Kevin',
+        lastName: 'Johnson',
+        age: 34,
+        address: {
+          street: '20 School st',
+          city: 'Lynn',
+          state: 'MA'
         },
-        {
-          firstName: 'Karen',
-          lastName: 'Williams',
-          age: 26,
-          address: {
-            street : '55 Mill St',
-            city : 'Miami',
-            state: 'FL'
-          },
-          image: 'https://placeimg.com/300/300/people/2',
-          isActive : false,
-          balance: 50,
-          registered: new Date('11/02/2016 10:30:00')
-        }
-  
-      ];
-      // this.addUser({
-      //   firstName: 'David',
-      //   lastName: 'Jackson',
-       
-      // })
-      this.loaded = true;
-    
-   this.showExtended = true;
+        // image: 'https://placeimg.com/300/300/people/1',
+        isActive: false,
 
-   this.setCurrentClasses();
-   this.setCurrentStyles();
+        registered: new Date('03/11/2018 08:30:00'),
+        hide: true
+      },
+      {
+        firstName: 'Karen',
+        lastName: 'Williams',
+        age: 26,
+        address: {
+          street: '55 Mill St',
+          city: 'Miami',
+          state: 'FL'
+        },
+        // image: 'https://placeimg.com/300/300/people/2',
+        isActive: false,
+
+        registered: new Date('11/02/2016 10:30:00'),
+        hide: true
+      }
+
+    ];
+    // this.addUser({
+    //   firstName: 'David',
+    //   lastName: 'Jackson',
+
+    // })
+    this.loaded = true;
+
+    //  this.showExtended = true;
+
+    //  this.setCurrentClasses();
+    //  this.setCurrentStyles();
   }
 
-  addUser(user: User){
-    this.users.push(user);
-  }
+  addUser(user: User) {
+    // unshift adds to the beginning of the array
+    this.users.unshift(this.user);
+    // uses todays date as registration date
+    this.user.registered = new Date();
 
-  setCurrentClasses(){
-    this.currentClasses = {
-    'btn-success' : this.enableAdd,
-    'big-text' : this.showExtended
+    this.users.unshift(this.user);
+
+    this.user = {
+      firstName: '',
+      lastName: '',
+      age: null,
+      address: {
+        street: '',
+        city: '',
+        state: ''
+
+      }
+
     }
   }
 
-  setCurrentStyles(){
-this.currentStyles = {
-  'padding-top' : this.showExtended ? '0': '40px',
-  'font-size' : this.showExtended ? '' : '40px'
-}
+  toggleHide(user: User) {
+    user.hide = !user.hide;
   }
+
+
+  onSubmit(e) {
+    console.log(123);
+
+    // prevents refresh when submitting form
+    e.preventDefault();
+  }
+
+  fireEvent(e) {
+    console.log(e.target.value);
+    console.log(e.type);
+  }
+
+  //   setCurrentClasses(){
+  //     this.currentClasses = {
+  //     'btn-success' : this.enableAdd,
+  //     'big-text' : this.showExtended
+  //     }
+  //   }
+
+  //   setCurrentStyles(){
+  // this.currentStyles = {
+  //   'padding-top' : this.showExtended ? '0': '40px',
+  //   'font-size' : this.showExtended ? '' : '40px'
+  // }
+  // }
 }
--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -9,8 +9,9 @@ export interface User {
     city?: string,
     state?: string
   }  
-  image?: string,
+  // image?: string,
   isActive?: boolean,
-  balance?: number,
-  registered?: any
+  // balance?: number,
+  registered?: any,
+  hide?: boolean
 }
