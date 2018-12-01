53a9c5f1b7def7c719c1085b2974a44644367b01
JOHNATHAN JOYCE <G00007904@gmit.ie>
2018-12-01 13:41:38 +0000

    Http Api working, Routing still to be added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/Forms';
 
+import { HttpClientModule} from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
 import { UsersComponent } from './components/users/users.component';
@@ -11,6 +13,9 @@ import {UserService} from './services/user.service';
 import {PostService} from './services/post.service';
 
 import { PostsComponent } from './components/posts/posts.component';
+import { PostFormComponent } from './components/post-form/post-form.component';
+
+
 
 @NgModule({
 
@@ -20,11 +25,13 @@ import { PostsComponent } from './components/posts/posts.component';
     UserComponent,
     UsersComponent,
     NavbarComponent,
-    PostsComponent
+    PostsComponent,
+    PostFormComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
 
   ],
   providers: [UserService, PostService],
